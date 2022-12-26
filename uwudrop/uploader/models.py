aa5dbd17466f9d828a8beee636ee6cc926c9016e
korneliuszw <27420848+korneliuszw@users.noreply.github.com>
2022-12-26 22:17:51 +0100

    uploader model

--- uwudrop/uploader/models.py
+++ uwudrop/uploader/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Uploader(models.Model):
+    public_ip = models.CharField(max_length=40)
+    cookie = models.TextField()
+
+class File(models.Model):
+    upload_path = models.TextField()
+    uploader = models.ForeignKey(Uploader, on_delete=models.CASCADE)
+    identifier = models.CharField(max_length=50, unique=True, primary_key=True)
+    delete_at = models.DateTimeField()
+    remaining_downloads = models.IntegerField(null=True)
