13c8c718efcf0de1103152c51c8bef8502ff6f5c
korneliuszw <27420848+korneliuszw@users.noreply.github.com>
2023-01-07 18:45:41 +0100

    admin panel add models

--- uwudrop/uploader/admin.py
+++ uwudrop/uploader/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from uploader.models import Upload, Uploader, FileUpload, IdentifierDictionary
 # Register your models here.
+
+@admin.register(Uploader)
+class UploaderAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Upload)
+class UploadAdmin(admin.ModelAdmin):
+    pass
+@admin.register(FileUpload)
+class FileUploadAdmin(admin.ModelAdmin):
+    pass
+@admin.register(IdentifierDictionary)
+class IdentifierDictionaryAdmin(admin.ModelAdmin):
+    pass
