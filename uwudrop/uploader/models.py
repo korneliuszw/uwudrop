62a3100f21c0f32d128189afd6caaf4bce7fd8ec
korneliuszw <27420848+korneliuszw@users.noreply.github.com>
2023-01-07 18:45:52 +0100

    update models and migrate

--- uwudrop/uploader/models.py
+++ uwudrop/uploader/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 class Uploader(models.Model):
     public_ip = models.CharField(max_length=40)
-    cookie = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
-class File(models.Model):
-    upload_path = models.TextField()
+class Upload(models.Model):
     uploader = models.ForeignKey(Uploader, on_delete=models.CASCADE)
     identifier = models.CharField(max_length=50, unique=True, primary_key=True)
-    delete_at = models.DateTimeField()
+    delete_at = models.DateTimeField(null=True)
     remaining_downloads = models.IntegerField(null=True)
+
+class FileUpload(models.Model):
+    file = models.FileField()
+    upload = models.ForeignKey(Upload, on_delete=models.CASCADE)
+
+class IdentifierDictionary(models.Model):
+    word = models.CharField(max_length = 10)
