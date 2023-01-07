9d47a4d3d5d5cc71a29c33c73197e254a3e49fb4
korneliuszw <27420848+korneliuszw@users.noreply.github.com>
2023-01-07 21:28:44 +0100

    default identifier

--- uwudrop/uploader/models.py
+++ uwudrop/uploader/models.py
@@ -8,10 +8,22 @@ class Uploader(models.Model):
 
 def maxDaysFromNow():
     return datetime.now() + timedelta(days=+settings.MAX_FILE_UPLOAD_DAYS)
+
+def generate_identifer():
+    word_ids = set()
+    if (settings.WORDS < 10):
+        logger.critical("set more words!")
+    # seed set until exactly 3 elements because every id must be unique
+    while len(word_ids) != 3:
+        word_ids.add(randint(1, settings.WORDS))
+    id = ""
+    for x in IdentifierDictionary.objects.filter(pk__in=word_ids):
+        id += x.word
+    return id
 class Upload(models.Model):
     uploader = models.ForeignKey(Uploader, on_delete=models.CASCADE)
-    identifier = models.CharField(max_length=50, unique=True, primary_key=True)
-    delete_at = models.DateTimeField(default=maxDaysFromNow())
+    identifier = models.CharField(max_length=50, unique=True, primary_key=True, default=generate_identifer)
+    delete_at = models.DateTimeField(default=maxDaysFromNow)
     password = models.CharField(max_length=80, null=True)
     remaining_downloads = models.IntegerField(null=True)
 
