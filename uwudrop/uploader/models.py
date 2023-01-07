30e836bb5182b4f6d78a0475e98af57ff0c8973c
korneliuszw <27420848+korneliuszw@users.noreply.github.com>
2023-01-07 21:18:35 +0100

    default date for delete_at

--- uwudrop/uploader/models.py
+++ uwudrop/uploader/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-
+from datetime import datetime, timedelta
+from django.conf import settings
 class Uploader(models.Model):
     public_ip = models.CharField(max_length=40)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
+def maxDaysFromNow():
+    return datetime.now() + timedelta(days=+settings.MAX_FILE_UPLOAD_DAYS)
 class Upload(models.Model):
     uploader = models.ForeignKey(Uploader, on_delete=models.CASCADE)
     identifier = models.CharField(max_length=50, unique=True, primary_key=True)
-    delete_at = models.DateTimeField()
+    delete_at = models.DateTimeField(default=maxDaysFromNow())
     password = models.CharField(max_length=80, null=True)
     remaining_downloads = models.IntegerField(null=True)
 
