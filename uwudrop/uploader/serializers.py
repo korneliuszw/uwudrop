2d36e935291a75f07b22eb435af167bbfefa5ffb
korneliuszw <27420848+korneliuszw@users.noreply.github.com>
2023-01-07 21:28:35 +0100

    serializer for upload

---
+++ uwudrop/uploader/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from uploader.models import maxDaysFromNow, Upload
+from django.conf import settings
+
+class UploadSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        if data['delete_at'] > maxDaysFromNow():
+            raise serializers.ValidationError(f"Date cannot be greater than {settings.MAX_FILE_UPLOAD_DAYS} days from request time")
+    class Meta:
+        model = Upload
+        fields = ['delete_at', 'password', 'remaining_downloads']
