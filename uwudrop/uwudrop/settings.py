79a38f6d3a01fa62a038c66ef174ef1058d17898
korneliuszw <27420848+korneliuszw@users.noreply.github.com>
2023-01-08 18:57:02 +0100

    CORS and CSRF for localhost frontend

--- uwudrop/uwudrop/settings.py
+++ uwudrop/uwudrop/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'uploader.apps.UploaderConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'uwudrop.urls'
@@ -124,5 +126,17 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173'
+]
+CORS_ALLOWED_HEADERS = [
+    'content-type'
+]
+CORS_ALLOW_CREDENTIALS = True
+
 WORDS = 10
 MAX_FILE_UPLOAD_DAYS = 7
+
